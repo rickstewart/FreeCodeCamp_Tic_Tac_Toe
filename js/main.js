066e2dfc31f06a79c5323825cab4e702bbd04fa7
rickstewart <rick.stewart@ns.sympatico.ca>
2016-05-10 14:18:48 -0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Rick Stewart on 05/10/2016.
+ */
+/**
+ * set JSHint not to flag these variables as 'unresolved variable'
+ * @param channelResponse.stream
+ * @param channelResponse.stream.channel
+ * @param data.logo
+ *
+ */
