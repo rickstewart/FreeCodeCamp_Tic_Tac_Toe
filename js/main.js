05ffe3b27e53eb249e968122607e04521da0bdc0
rickstewart <rick.stewart@ns.sympatico.ca>
2016-05-10 20:28:48 -0300

    Initial commit

--- js/main.js
+++ js/main.js
@@ -9,6 +9,9 @@
  *
  */
 (function() {
-    var paper = new Raphael(document.getElementById('canvas_container'), 500, 500);
-    paper.drawnLine(100, 100, 200, 200, wobble).attr({stroke: 'red'});
+    var svg = new Raphael(document.getElementById('canvas_container'), 500, 500);
+    var circle = svg.drawnCircle(250, 250, 100, 5);
+    circle.attr({"stroke":"red", "stroke-width": 5});
+    var line= svg.drawnRect(1, 1, 100, 100, 5);
+    line.attr({"stroke":"red", "stroke-width": 5});
 })();
