e996c84d64cf3631f7b65d4eb1322ef6fc6a57b0
rickstewart <rick.stewart@ns.sympatico.ca>
2016-05-14 18:30:49 -0300

    Raphael figured out, building game board and pieces.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
 /**
  * Created by Rick Stewart on 05/10/2016.
  */
+
 /**
  * set JSHint not to flag these variables as 'unresolved variable'
- * @param channelResponse.stream
- * @param channelResponse.stream.channel
- * @param data.logo
- *
+ * @param Raphael
  */
 
-/*
+/**
  :::: This shows how the Game Board is mapped - zero indexed for array indexing. ::::
 
  0 | 1 | 2
@@ -34,6 +32,7 @@
     var moves;                                   // holds player and AI game moves.
     var coordCenterSquare;                       // holds map of (x, y) of center of each board square.
     var lastClickedSquare;                       // holds the name of the last clicked game board square.
+    var playerPiece;                             // holds player's choice of playing X ro O.
 
     /* function calculateBoardDimensions() updates the variables holding the board dimensions, as well as
      the dimensions of the container holding the board. */
@@ -81,7 +80,6 @@
 
     /* function drawBoard() paints the graphics unto the browser window. */
     function drawBoard() {
-        moves = ['U', 'X', 'O', 'U', 'O', 'O','X', 'X', 'X'];
         paper.drawnLine(20, 132, 380, 132, 5).attr({'stroke': 'red', 'stroke-width': 2});  // draw 4 lines of game board.
         paper.drawnLine(20, 262, 380, 264, 5).attr({'stroke': 'red', 'stroke-width': 2});
         paper.drawnLine(132, 20, 132, 380, 5).attr({'stroke': 'red', 'stroke-width': 2});
@@ -96,7 +94,8 @@
 
     /* function init() runs at the beginning of the program to initialize variables and settings. */
     function init() {
-        moves = ['U', 'U', 'U', 'U', 'U', 'U','U', 'U', 'U'];   // U is unoccupied square, one for each of 9 positions.
+        moves = ['U', 'U', 'U', 'U', 'U', 'U', 'U', 'U', 'U'];   // U is unoccupied square, one for each of 9 positions.
+        playerPiece = '';
         coordCenterSquare = [[60, 60], [188, 60], [316, 60], [60, 188], [188, 188], [318, 188], [60, 316], [188, 318], [318, 318]];
         calculateBoardDimensions();
         paper = Raphael('canvas_container');     // create new Raphael object.
@@ -106,11 +105,37 @@
         svg.removeAttribute('height');           // Raphael sets an absolute height on svg, removed for proper scaling.
         drawBoard();
     }
+    
+    /* Listener attached to radio button group.  When fired the group is disabled from further changes. Choice highlighted. */
+    $(':radio').click(function(e){
+        document.getElementById('radioO').disabled = true;
+        document.getElementById('radioX').disabled = true;
+        if(e.currentTarget.id === 'radioX') {
+            playerPiece = 'X';
+            document.getElementById('X').style.backgroundColor='#FFA500';
+            document.getElementById('X').style.color='black';
+            document.getElementById('X').style.border='solid 1px red';
+            document.getElementById('X').style.borderRadius='1.0em';
+            document.getElementById('X').style.padding='3px';
+        }
+        else {
+            playerPiece = 'O';
+            document.getElementById('O').style.backgroundColor='#FFA500';
+            document.getElementById('O').style.color='black';
+            document.getElementById('O').style.border='solid 1px red';
+            document.getElementById('O').style.borderRadius='1.0em';
+            document.getElementById('O').style.padding='3px';
 
-    /* Listener added to detect when a player has made a move, and which square was clicked. */
-    $('#canvas_container').on('click', '.clickPad', function(e) {  // syntax for dynamically created content.
-        lastClickedSquare = e.currentTarget.id;
-        drawX(lastClickedSquare);
+        }
+    });
+
+    /* Listener added to detect when a player has made a move, and which square was clicked. Update game board and record move. */
+    $('#canvas_container').on('click', '.clickPad', function(e) {  // syntax for Listener on dynamically created content.
+        lastClickedSquare = e.currentTarget.id;                    // get id of clicked square.
+        if(playerPiece !== '') {                                   // test to make sure player picked a marker, else board disabled.
+            moves[lastClickedSquare] = playerPiece;                // record move.
+            playerPiece === 'X' ? drawX(lastClickedSquare) : drawO(lastClickedSquare);  // update game board.
+        }
     });
 
     /* Listener added to detect changes to the viewport size and adjust board accordingly. */
